a38fff44f6fa29ac05c15999a78330c32ff1249a
Himu <himangshisaha1@gmail.com>
2024-04-14 21:01:55 +0530

    Daily-Route-Planner-app up to Homepage update 4/14/2024

--- app/src/main/java/com/example/dailyrouteplanner/ProfileActivity.java
+++ app/src/main/java/com/example/dailyrouteplanner/ProfileActivity.java
@@ -2,13 +2,43 @@ package com.example.dailyrouteplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3,btn4,btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btn1=findViewById(R.id.button5);
+        btn2=findViewById(R.id.button6);
+        btn3=findViewById(R.id.button8);
+        btn4=findViewById(R.id.button7);
+        btn5=findViewById(R.id.button10);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,LoginActivity.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,LoginActivity.class));
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,HomeActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dailyrouteplanner/TripActivity.java
+++ app/src/main/java/com/example/dailyrouteplanner/TripActivity.java
@@ -2,7 +2,13 @@ package com.example.dailyrouteplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TripActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class TripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+        EditText editTextSource = findViewById(R.id.source);
+        EditText editTextDest = findViewById(R.id.destination);
+        Button btnS = findViewById(R.id.btnSubmit);
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String source = editTextSource.getText().toString();
+                String dest =editTextDest.getText().toString();
+                if(source.equals("") || dest.equals("")){
+                    Toast.makeText(getApplicationContext(),"Enter the source and destination both.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Uri uri = Uri.parse("https://www.google.com/maps/dir/" + source + "/" + dest);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    intent.setPackage("com.google.android.apps.maps");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
